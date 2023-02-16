803b12041faf50be028df9880c1a5489b9c9a24a
Nishat Humaira <nishathumaira@Nishats-MacBook-Pro.local>
2023-02-16 14:50:47 +0000

    Loops

---
+++ Test6.java
@@ -0,0 +1,19 @@
+class Test6{
+	
+	
+	public static void main(String[] args){
+		
+		int counter = 1; // Initialisation
+		
+		while(counter <= 10){ // Condition
+		
+			System.out.println("Welcome to Java");
+				
+			//counter++; // Increment
+		}
+		
+	}
+
+
+}
+
