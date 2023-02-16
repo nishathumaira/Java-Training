d9d86c3a7bef960bc16fb667fa3a04d9e82db0a9
Nishat Humaira <nishathumaira@Nishats-MacBook-Pro.local>
2023-02-16 14:49:26 +0000

    Day 1

---
+++ Test7.java
@@ -0,0 +1,25 @@
+class Test7{
+	
+	
+	public static void main(String[] args){
+		
+		int counter = 1; // Initialisation
+		
+		while(counter <= 10){ // Condition
+		
+			if(counter == 5){
+				
+				break;
+			}
+			
+			System.out.println("Welcome to Java");
+				
+			counter++; // Increment
+		}
+		
+		
+		
+	}
+
+
+}
