957586292886171796e380cd975ab7f8e1ea626c
Nishat Humaira <nishathumaira@Nishats-MacBook-Pro.local>
2023-02-16 14:48:57 +0000

    First Program

---
+++ Practice1.java
@@ -0,0 +1,27 @@
+class Practice1{
+
+
+	public static void main(String[] args){
+
+		String firstName = "Nishat";
+
+		System.out.println(firstName);
+		
+		System.out.println("My first name is "+firstName);
+				
+		int a = 10;
+		
+		int b = 20;
+	
+		System.out.println("Sum of "+a+" and "+b+" is "+(a+b));
+		
+		//float x = 10.5; // Error
+		
+		float x = 10.566F; // 
+		
+		System.out.println(x);
+		
+	}
+
+
+}
