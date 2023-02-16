f53d9555ea6ac717fac03bba6106dc135e71eb1b
Nishat Humaira <nishathumaira@Nishats-MacBook-Pro.local>
2023-02-16 14:51:17 +0000

    Basics

---
+++ Test2.java
@@ -0,0 +1,28 @@
+class Test2{
+
+   public static void main(String[] args){
+	
+	 int x = 10;
+
+	 long y = 100;
+	 
+	 int z = x + y;
+	 
+	 //long z = x + y;
+	 
+	 //int z = (int) (x + y);
+	 
+	 System.out.println(z);
+	 
+	 
+	/* byte x2 = 100;
+	 
+	 int a1 = x2 + x;
+	 
+	 System.out.println(a1);
+	 
+	 */
+	
+   }
+   
+}
