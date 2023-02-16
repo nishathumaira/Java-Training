9cb0a15018964914fc5a3877775a389c4b3c50f8
Nishat Humaira <nishathumaira@Nishats-MacBook-Pro.local>
2023-02-16 14:54:17 +0000

    Day 1

---
+++ Test.java
@@ -0,0 +1,22 @@
+class Test{
+
+   public static void main(String[] args){
+	
+	int age;
+	System.out.println(age);
+	String name;
+	System.out.println(name);
+	
+	/*
+	String firstName = "Nishat"; // Declaration and Initialization
+	
+	String emailId; // Declaration 
+	
+	emailId = "nish@gmail.com"; // Initialization
+	*/
+  }   
+
+
+}
+
+// NOTE: All local variables in Java must be initialized before use
