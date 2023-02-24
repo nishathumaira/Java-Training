f11be57b8b18a4f66195a71d96c77f507976adff
Nishat Humaira <nishathumaira@nishats-mbp.home>
2023-02-24 00:11:16 +0000

    Exercise 7

--- LambdaExercise.java
+++ LambdaExercise.java
@@ -11,3 +11,55 @@ public class LambdaExercise {
             .forEach(year -> System.out.println(year));
     }
 }
+
+
+class Employee implements Comparable<Employee> {
+
+    private String name;
+    private int age;
+    private double salary;
+
+    // Constructor
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Employee{" +"name='" + name + '\'' +", age=" + age +", salary=" + salary +'}';
+    }
+
+    // compareTo method for comparing employees by name
+    @Override
+    public int compareTo(Employee otherEmployee) {
+        return this.name.compareTo(otherEmployee.getName());
+    }
+}
